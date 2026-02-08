1b14d9f976f81f1c884b5d4befd93aea9e52e298
Nils1024 <78668364+Nils1024@users.noreply.github.com>
2026-02-08 21:34:07 +0100

    Fixed resizing bug of the textbox and started with the main game

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,9 +5,8 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$FadeTransition/AnimationPlayer.play("fade_out")
-	textbox.offset = Vector2(200, 200)
+	textbox.offset = Vector2(400, 200)
 	
 	textbox.queue_text("Hello, welcome to Godot!")
-	textbox.queue_text("we are going")
-	textbox.queue_text("123456778")
-	textbox.queue_text("abcdefghh")
+	textbox.queue_text("In this small tutorial, you will build a 2D game with a player that can run and jump around.")
+	textbox.queue_text("First of all I want to show you all the important areas of Godot.")
