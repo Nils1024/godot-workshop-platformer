3bfdfda5111f35b3600372c5608aee1034dc95ac
Nils1024 <78668364+Nils1024@users.noreply.github.com>
2026-02-09 23:05:10 +0100

    Fixed texbox wrap and continued on the tutorial by adding more text and the ability to open the node selection

--- scripts/game.gd
+++ scripts/game.gd
@@ -7,8 +7,11 @@ enum GameState {
 	NODE_INTRO,
 	FILE_INTRO,
 	INSPECTOR_INTRO,
-	WORKSPACE_INTRO
-}
+	WORKSPACE_INTRO,
+	CREATE_ROOT_NODE_1,
+	CREATE_ROOT_NODE_2,
+	CREATE_ROOT_NODE_3,
+}	
 
 var current_game_state = GameState.INTRO
 
@@ -26,20 +29,33 @@ func _process(delta: float) -> void:
 		match current_game_state:
 			GameState.INTRO:
 				$Panel.show()
-				textbox.queue_text("This is your node editor")
+				textbox.queue_text("Nodes and scenes are game parts. They will be listed here.")
 				current_game_state = GameState.NODE_INTRO
 			GameState.NODE_INTRO:
 				$Panel.hide()
 				$Panel2.show()
-				textbox.queue_text("This is your file explorer")
+				textbox.queue_text("This is your file explorer. Here you will find all your files and folders (e.g. images, scenes and code).")
 				current_game_state = GameState.FILE_INTRO
 			GameState.FILE_INTRO:
 				$Panel2.hide()
 				$Panel3.show()
-				textbox.queue_text("This is your inspector")
+				textbox.queue_text("This is your inspector. When you selected a node, you can edit all its properties in here.")
 				current_game_state = GameState.INSPECTOR_INTRO
 			GameState.INSPECTOR_INTRO:
 				$Panel3.hide()
 				$Panel4.show()
-				textbox.queue_text("This is your workspace")
+				textbox.queue_text("This is your Main viewport. Here you can see what your game looks like.")
 				current_game_state = GameState.WORKSPACE_INTRO
+			GameState.WORKSPACE_INTRO:
+				$Panel4.hide()
+				textbox.queue_text("Now you know what you probably need the most. Let's start creating your first game!")
+				current_game_state = GameState.CREATE_ROOT_NODE_1
+			GameState.CREATE_ROOT_NODE_1:
+				textbox.queue_text("In the Scene section, choose Other Node and select Node.")
+				current_game_state = GameState.CREATE_ROOT_NODE_2
+			GameState.CREATE_ROOT_NODE_3:
+				$TextureRect2.show()
+				
+
+func _on_other_node_button_pressed() -> void:
+	current_game_state = GameState.CREATE_ROOT_NODE_3
